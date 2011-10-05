00208734ee0552f359ab235af57a62262838fea4
Marco A. Martinez <mam002@ucsd.edu>
2011-10-05 14:15:50 -0700

    Changes accepted

--- src/leadtheway/policy/testing.py
+++ src/leadtheway/policy/testing.py
@@ -7,17 +7,23 @@ from plone.testing import z2
 
 from zope.configuration import xmlconfig
 
-class leadthewayPolicy(PloneSandboxLayer):
+
+class LeadTheWayPolicy(PloneSandboxLayer):
 
     defaultBases = (PLONE_FIXTURE,)
-    
+
     def setUpZope(self, app, configurationContext):
         # Load ZCML
-        import leadtheway.policy
-        xmlconfig.file('configure.zcml', leadtheway.policy, context=configurationContext)
-        
+        import leadtheway.policy as package
+        xmlconfig.file('configure.zcml', package, context=configurationContext)
+
     def setUpPloneSite(self, portal):
         applyProfile(portal, 'leadtheway.policy:default')
 
-LEADTHEWAY_POLICY_FIXTURE = leadthewayPolicy()
-LEADTHEWAY_POLICY_INTEGRATION_TESTING = IntegrationTesting(bases=(LEADTHEWAY_POLICY_FIXTURE,), name="leadtheway:Integration")
+
+LEADTHEWAY_POLICY_FIXTURE = LeadTheWayPolicy()
+
+LEADTHEWAY_POLICY_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(LEADTHEWAY_POLICY_FIXTURE,),
+    name='leadtheway:Integration'
+    )
